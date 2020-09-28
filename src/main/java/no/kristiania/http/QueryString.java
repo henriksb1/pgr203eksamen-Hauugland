7055ffdf3aa6752e6639d5b855cd9657689a6544
Petter Haugland <petterhaugland@gmail.com>
2020-09-28 18:10:04 +0200

    start on QueryString class, test "shouldRetrieveQueryParameter" (red);

---
+++ src/main/java/no/kristiania/http/QueryString.java
@@ -0,0 +1,12 @@
+package no.kristiania.http;
+
+public class QueryString {
+
+    public QueryString(String queryString) {
+
+    }
+
+    public String getParameter(String name) {
+        return null;
+    }
+}
