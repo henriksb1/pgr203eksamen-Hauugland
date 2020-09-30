911b0cd3905fe654a0276b9c39df052f24cba865
Petter Haugland <petterhaugland@gmail.com>
2020-09-30 16:43:17 +0200

    Done with HttpMessage for now, start on POST

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -3,13 +3,11 @@ package no.kristiania.http;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HttpClient {
-    private final int responseCode;
-    private Map<String, String> responseHeaders = new HashMap<>();
+
     private String responseBody;
+    private HttpMessage responseMessage;
 
     public HttpClient(String hostname, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(hostname, port);
@@ -18,40 +16,9 @@ public class HttpClient {
         requestMessage.setHeader("Host", hostname);
         requestMessage.write(socket);
 
-        String line = readLine(socket);
+        responseMessage = HttpMessage.read(socket);
 
-        System.out.println(line);
-        String[] responseLineParts = line.split(" ");
-        responseCode = Integer.parseInt(responseLineParts[1]);
-
-        String headerLine;
-        while(!(headerLine = readLine(socket)).isEmpty()){
-            System.out.println(headerLine);
-            int colonPos = headerLine.indexOf(':');
-            String name = headerLine.substring(0, colonPos);
-            String value = headerLine.substring(colonPos + 1).trim();
-            responseHeaders.put(name, value);
-        }
-
-        int contentLength = Integer.parseInt(getResponseHeader("Content-Length"));
-        StringBuilder body = new StringBuilder();
-        for (int i = 0; i < contentLength; i++) {
-            body.append((char)socket.getInputStream().read());
-        }
-        responseBody = body.toString();
-    }
-
-    public static String readLine(Socket socket) throws IOException {
-        StringBuilder line = new StringBuilder();
-        int c;
-        while((c = socket.getInputStream().read()) != -1){
-            if(c == '\r'){
-                socket.getInputStream().read();
-                break;
-            }
-            line.append((char) c);
-        }
-        return line.toString();
+        responseBody = HttpMessage.readBody(socket, getResponseHeader("Content-Length"));
     }
 
     public static void main(String[] args) throws IOException {
@@ -63,11 +30,13 @@ public class HttpClient {
 
 
     public int getResponseCode() {
-        return  responseCode;
+        String[] responseLineParts = responseMessage.getStartLine().split(" ");
+        int responseCode = Integer.parseInt(responseLineParts[1]);
+        return responseCode;
     }
 
     public String getResponseHeader(String headerName) {
-        return responseHeaders.get(headerName);
+        return responseMessage.getHeader(headerName);
     }
 
     public String getResponseBody() {
